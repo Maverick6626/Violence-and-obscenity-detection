ade1295a6b636c20e6bf2c344ff4e994fb99341b
Anurag Pandey <137084874+Maverick6626@users.noreply.github.com>
2025-08-01 15:15:59 +0530

    Update framerate.py

--- cnn with attention/framerate.py
+++ cnn with attention/framerate.py
@@ -1,5 +1,4 @@
 from decord import VideoReader, cpu
-import model_utils
 
 # Read 1 frame per second from the video
 def framerate_extract(video_path):
@@ -11,13 +10,3 @@ def framerate_extract(video_path):
     frame_indices = list(range(0, total_frames, jump))
     frames = vr.get_batch(frame_indices).asnumpy()
     return frames
-
-if __name__ == '__main__':
-    import time
-    video_path = 'test.mov'
-    start_time = time.time()
-    results = model_utils.predict(framerate_extract, video_path)
-    end_time = time.time()
-
-    print(f'Results: {results}')
-    print(f'Time taken: {end_time - start_time:.2f} seconds')
