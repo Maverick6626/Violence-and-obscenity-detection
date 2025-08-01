96671c2fb60b3bad58223c0e91cc4f2bfd77e64b
Anurag Pandey <137084874+Maverick6626@users.noreply.github.com>
2025-08-01 15:16:31 +0530

    Update keyframe.py

--- cnn with attention/keyframe.py
+++ cnn with attention/keyframe.py
@@ -1,6 +1,5 @@
 import av
 import numpy as np
-import model_utils
 
 def extract_keyframes(video_path):
     container = av.open(video_path)
@@ -14,13 +13,3 @@ def extract_keyframes(video_path):
     container.close()
     frames = np.array(frames)
     return frames
-
-if __name__ == '__main__':
-    import time
-    video_path = 'test.mov'
-    start_time = time.time()
-    results = model_utils.predict(extract_keyframes, video_path)
-    end_time = time.time()
-
-    print(f'Results: {results}')
-    print(f'Time taken: {end_time - start_time:.2f} seconds')
